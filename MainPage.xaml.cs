ee6307d7a3a9ed37e71161142e75c3cedd382d5e
Ryan Campbell <ryan.campbell@qesolutionsllc.com>
2023-07-03 15:05:42 -0400

    Converted this to a MAUI app

---
+++ MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.ObjectModel;
+
+namespace EDHPlayerTracker;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<Player> Players { get; set; } = new ObservableCollection<Player>()
+    {
+        new Player(),
+        new Player(),
+        new Player(),
+        new Player(),
+    };
+
+    public MainPage()
+    {
+        InitializeComponent();
+        BindingContext = this;
+    }
+
+    private void SetActive(int i)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == j)
+            {
+                Players[j].Activate();
+            }
+            else
+            {
+                Players[j].Deactivate();
+            }
+        }
+    }
+
+    private void Timer_Clicked(object sender, EventArgs e)
+    {
+        var i = int.Parse((sender as Button).AutomationId);
+        SetActive(i);
+    }
+
+    private void HpUp_Clicked(object sender, EventArgs e)
+    {
+        var i = int.Parse((sender as Button).AutomationId);
+        ++Players[i].Health;
+    }
+
+    private void HpDown_Clicked(object sender, EventArgs e)
+    {
+        var i = int.Parse((sender as Button).AutomationId);
+        --Players[i].Health;
+    }
+}
+
