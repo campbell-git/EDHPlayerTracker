4fb531b53cfcf1de201a6458030b2568ff570c11
Ryan Campbell <ryan.campbell@qesolutionsllc.com>
2023-07-03 13:28:53 -0400

    Initial commit

---
+++ EDHPlayerTracker/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace EDHPlayerTracker
+{
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<Player> Players { get; set; } = new ObservableCollection<Player>() {
+            new Player(),
+            new Player(),
+            new Player(),
+            new Player(),
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+        }
+
+        private void SetActive(int i)
+        {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i == j)
+            {
+                Players[j].Activate();
+            }
+            else
+            {
+                Players[j].Deactivate();
+            }
+        }
+        }
+
+        private void Timer_Clicked(object sender, EventArgs e)
+        {
+            var i = int.Parse((sender as Button).AutomationId);
+            SetActive(i);
+        }
+
+        private void HpUp_Clicked(object sender, EventArgs e)
+        {
+            var i = int.Parse((sender as Button).AutomationId);
+            ++Players[i].Health;
+        }
+
+        private void HpDown_Clicked(object sender, EventArgs e)
+        {
+            var i = int.Parse((sender as Button).AutomationId);
+            --Players[i].Health;
+        }
+    }
+}
